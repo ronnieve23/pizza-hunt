2d336689a58750ee987176b4d876717ab2aeb75f
Ronnieve23 <ronnieve_romero@yahoo.com>
2021-10-17 09:25:59 -0400

    added validations

--- models/Comment.js
+++ models/Comment.js
@@ -3,16 +3,19 @@ const dateFormat = require('../utils/dateFormat');
 
 const ReplySchema = new Schema(
   {
-    // set custom id to avoid confusion with parent comment's _id field
+    // set custom id to avoid confusion with parent comment _id
     replyId: {
       type: Schema.Types.ObjectId,
       default: () => new Types.ObjectId()
     },
     replyBody: {
-      type: String
+      type: String,
+      required: true
     },
     writtenBy: {
-      type: String
+      type: String,
+      required: true,
+      trim: true
     },
     createdAt: {
       type: Date,
@@ -22,18 +25,20 @@ const ReplySchema = new Schema(
   },
   {
     toJSON: {
-      virtuals: true,
       getters: true
     }
   }
 );
+
 const CommentSchema = new Schema(
   {
     writtenBy: {
-      type: String
+      type: String,
+      required: true
     },
     commentBody: {
-      type: String
+      type: String,
+      required: true
     },
     createdAt: {
       type: Date,
@@ -51,10 +56,10 @@ const CommentSchema = new Schema(
     id: false
   }
 );
+
 CommentSchema.virtual('replyCount').get(function() {
   return this.replies.length;
 });
-
 
 const Comment = model('Comment', CommentSchema);
 
